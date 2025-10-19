3fb8ea656aa6c2aa66ab2b9296e4eb218808c0bc
rafael luis <rafael.luis.prf@gmail.com>
2025-10-19 17:05:19 +0100

    feat: new source set for integration tests

--- PLS/CA2/Part2_Alternative/src/main/java/payroll/Employee.java
+++ PLS/CA2/Part2_Alternative/src/main/java/payroll/Employee.java
@@ -7,16 +7,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-class Employee {
+public class Employee {
 
 	private @Id @GeneratedValue Long id;
 	private String firstName;
 	private String lastName;
 	private String role;
 
-	Employee() {}
+	public Employee() {}
 
-	Employee(String firstName, String lastName, String role) {
+	public Employee(String firstName, String lastName, String role) {
 
 		this.firstName = firstName;
 		this.lastName = lastName;
