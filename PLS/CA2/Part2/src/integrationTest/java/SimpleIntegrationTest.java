338cd8d4706f648fe388998f39da1aaa0039d846
SantiagoAzevedo <1201623@isep.ipp.pt>
2025-10-19 06:51:04 +0100

    Creating integration tests and respective task

---
+++ PLS/CA2/Part2/src/integrationTest/java/SimpleIntegrationTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleIntegrationTest {
+
+    @Test
+    void basicIntegrationTest() {
+        System.out.println("Test is being analysed");
+        assertTrue(true, "It passed");
+    }
+}
