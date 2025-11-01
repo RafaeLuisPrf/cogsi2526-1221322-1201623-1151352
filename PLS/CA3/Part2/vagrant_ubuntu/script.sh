052d84a998cd8258c13e4b12150238a6923cbd8d
SantiagoAzevedo <1201623@isep.ipp.pt>
2025-11-01 16:33:16 +0000

    Merge changes

---
+++ PLS/CA3/Part2/vagrant_ubuntu/script.sh
@@ -0,0 +1,11 @@
+# Update package list
+sudo apt-get update -y
+
+# Upgrade all packages
+sudo apt-get upgrade -y
+
+# Install git
+sudo apt-get install git -y
+
+# Install java
+sudo apt install openjdk-17-jdk -y
