f7dc9c3f570dbf9b9c722793d6273f40fb7e0f43
rafael luis <rafael.luis.prf@gmail.com>
2025-11-07 15:43:49 +0000

    Feat: Addition to ReadMe and Ansible local provision created

--- PLS/CA4/Part1/vagrant_ubuntu/script.sh
+++ PLS/CA4/Part1/vagrant_ubuntu/script.sh
@@ -1,11 +1,16 @@
 # Update package list
-sudo apt-get update -y
+
+# Speed up apt: use a faster mirror (e.g., Portugal or nearest country)
+sudo sed -i 's|http://archive.ubuntu.com/ubuntu|http://pt.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list
+
+# Update package list only if it's older than 1 day (avoids redundant downloads)
+if [ $(find /var/lib/apt/lists -name "*_Packages" -mtime +1 | wc -l) -gt 0 ]; then
+  sudo apt-get update --quiet
+fi
+
 
 # Upgrade all packages
 sudo apt-get upgrade -y
 
 # Install git
 sudo apt-get install git -y
-
-# Install java
-sudo apt install openjdk-17-jdk -y
