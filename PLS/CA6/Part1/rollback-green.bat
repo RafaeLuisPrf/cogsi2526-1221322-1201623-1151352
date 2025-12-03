5ad1bbd97d56eb9c398d1b5cd4850d670e7206d1
SantiagoAzevedo <1201623@isep.ipp.pt>
2025-12-03 15:27:44 +0000

    Changes on rollback playbook path

--- PLS/CA6/Part1/rollback-green.bat
+++ PLS/CA6/Part1/rollback-green.bat
@@ -2,9 +2,9 @@
 
 set JENKINS_USER=SantiagoAzevedo
 set JENKINS_TOKEN=""
-set STABLE_TAG=stable-v45
+set STABLE_TAG=stable-v5
 
-set INVENTORY=/mnt/c/Users/Admin/Documents/GitHub/cogsi2526-1221322-1201623-1151352/PLS/CA6/Part1/inventoryWsl.ini
-set PLAYBOOK=/mnt/c/Users/Admin/Documents/GitHub/cogsi2526-1221322-1201623-1151352/PLS/CA6/Part1/rollback.yml
+set INVENTORY=/mnt/c/Users/Admin/Documents/COGSI/cogsi2526-1221322-1201623-1151352/PLS/CA6/Part1/inventoryWsl.ini
+set PLAYBOOK=/mnt/c/Users/Admin/Documents/COGSI/cogsi2526-1221322-1201623-1151352/PLS/CA6/Part1/rollback.yml
 
 wsl bash -c "ansible-playbook -i %INVENTORY% %PLAYBOOK% --limit green --extra-vars 'jenkins_user=%JENKINS_USER% jenkins_token=%JENKINS_TOKEN% stable_tag=%STABLE_TAG%'"
