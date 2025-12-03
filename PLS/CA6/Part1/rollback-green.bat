c8579d8e26c7bf0df81ad6828a96145e3aa68974
SantiagoAzevedo <1201623@isep.ipp.pt>
2025-12-03 11:22:42 +0000

    CA6 Part1 implementation

---
+++ PLS/CA6/Part1/rollback-green.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set JENKINS_USER=SantiagoAzevedo
+set JENKINS_TOKEN=11fe1bbdf0b89b0b1f13bfe8d568fa4a74
+set STABLE_TAG=stable-v45
+
+set INVENTORY=/mnt/c/Users/Admin/Documents/GitHub/cogsi2526-1221322-1201623-1151352/PLS/CA6/Part1/inventoryWsl.ini
+set PLAYBOOK=/mnt/c/Users/Admin/Documents/GitHub/cogsi2526-1221322-1201623-1151352/PLS/CA6/Part1/rollback.yml
+
+wsl bash -c "ansible-playbook -i %INVENTORY% %PLAYBOOK% --limit green --extra-vars 'jenkins_user=%JENKINS_USER% jenkins_token=%JENKINS_TOKEN% stable_tag=%STABLE_TAG%'"
